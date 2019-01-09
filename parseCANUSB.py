13a477796b5db48ce0d09ab36e31529c88179fb4
Andrey Vinogradov <andrey.vinogradov@teplomonitor.ru>
2019-01-09 17:53:10 +0300

    add KSE CANbus protocol support

--- parseCANUSB.py
+++ parseCANUSB.py
@@ -21,10 +21,10 @@ def getHeaderLen(headerType):
 	if headerType not in CANHeaderIDE: return
 	
 	IDE = CANHeaderIDE[headerType]
-	if   IDE == 'STANDARD': return 4
+	if   IDE == 'STANDARD': return 3
 	elif IDE == 'EXTENDED': return 8
 
-def prepareTable():
+def prepareSmartNetTable():
 	t = PrettyTable([	'T',
 						'dT',
 						'Header',
@@ -40,7 +40,7 @@ def prepareTable():
 	t.align['Body'] = 'l'
 	return t
 
-def parseCANUSBLine(line):
+def parseSmartNetCANUSBLine(line):
 	headerType, line = cutFromLine(line, 1)
 	headerLen = getHeaderLen(headerType)
 	if headerLen != 8: return None, None, None
@@ -56,7 +56,7 @@ def parseCANUSBLine(line):
 	
 	return header, body, int(timestamp, 16)
 
-def parseHeader(header):
+def parseSmartNetHeader(header):
 	if len(header) != 4: return None, None, None, None
 
 	flag    = header[0][0]
@@ -72,29 +72,19 @@ def parseHeader(header):
 		int(type, 16)
 	)
 	
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Parse file name')
-	parser.add_argument('File', metavar='MyFile', nargs='+',
-					help='file with CAN log')
-	
-	arg = parser.parse_args()
-	FileToParse = arg.File[0]
-	
-	with open(FileToParse, 'r') as Input: content = Input.readlines()
-	
-	t = prepareTable()
-	
+def parseSmartNetProtocol(content):
+	t = prepareSmartNetTable()
+
 	oldTimestamp = 0
-	
 	for line in content:
-		header, body, timestamp = parseCANUSBLine(line)
+		header, body, timestamp = parseSmartNetCANUSBLine(line)
 		if not header: continue
 		
 		(	headerFlag,
 			headerFunction,
 			headerID,
 			headerType
-		) = parseHeader(header)
+		) = parseSmartNetHeader(header)
 		
 		if headerFlag == None: continue
 		
@@ -115,7 +105,102 @@ if __name__ == "__main__":
 					'TODO',
 					'TODO'
 				])
+	with open('Output.txt', 'w') as Output: Output.write(t.get_string())
+	
+	
+def prepareKseTable():
+	t = PrettyTable([	'T',
+						'dT',
+						'Header',
+						'module',
+						'busId',
+						'Body',
+						'Parsed header',
+						'Parsed body'
+					])
+	t.align = 'r'
+	t.align['Body'] = 'l'
+	return t
+	
+def parseKseCANUSBLine(line):
+	headerType, line = cutFromLine(line, 1)
+	headerLen = getHeaderLen(headerType)
+	
+	if headerLen != 3: return None, None, None
+	
+	header, line = cutFromLine(line, headerLen)
+	
+	bodySize, line = cutFromLine(line, 1)
+	bodySize = int(bodySize, 16)
+	body, line = cutFromLine(line, bodySize * 2)
+	body = splitAt(body, 2)
+	timestamp, line = cutFromLine(line, 4)
+	
+	return header, body, int(timestamp, 16)
+
+	
+def parseKseHeader(header):
+	header = int(header, 16)
+	
+	module = (header >> 7) & 0x0F
+	busId  = (header & 0x7F)
+	
+	return module, busId
+	
+def getKseModuleName(module):
+    return {
+        0x03: 'Kessel'   ,
+        0x06: 'Bedien'   ,
+        0x09: 'Manager'  ,
+        0x0A: 'Heizmodul',
+        0x0C: 'Mischer'  ,
+		0x0D: 'CoCo'     ,
+    }.get(module, 'None')    # 5 is default if module not found
+	
+	
+def parseKseProtocol(content):
+	t = prepareKseTable()
+	oldTimestamp = 0
+	for line in content:
+		header, body, timestamp = parseKseCANUSBLine(line)
+		if not header: continue
+		
+		module, busId = parseKseHeader(header)
+		
+		bodyStr   = ' '.join(body)
+		
+		delta = timestamp - oldTimestamp
+		oldTimestamp = timestamp
+		
+		parsedHeader = getKseModuleName(module)
+		
+		t.add_row([	timestamp,
+					delta,
+					header,
+					format(module, '#04x'),
+					busId,
+					bodyStr,
+					parsedHeader,
+					'TODO'
+				])
 	
 	with open('Output.txt', 'w') as Output: Output.write(t.get_string())
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Parse file name')
+	parser.add_argument('File', metavar='MyFile', nargs='+',
+					help='file with CAN log')
+	
+	arg = parser.parse_args()
+	FileToParse = arg.File[0]
+	
+	with open(FileToParse, 'r') as Input: content = Input.readlines()
+	
+	if False:
+		parseSmartNetProtocol(content)
+	else:
+		parseKseProtocol(content)
+	
 	
 	print 'Done'
