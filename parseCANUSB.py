cb6081e82e8864d0294a2e0fd1d546a97ab1f1da
Andrey.Vinogradov <andrey.vinogradov@teplomonitor.ru>
2022-04-17 17:47:39 +0300

    parseCANUSB.py: cosmetic fix

--- parseCANUSB.py
+++ parseCANUSB.py
@@ -6,13 +6,32 @@ from kseParser import parseKseProtocol
 from smartNetParser import parseSmartNetProtocol
 
 	
-def main():
-	parser = argparse.ArgumentParser(description='Parse file name')
-	parser.add_argument('File', metavar='MyFile', nargs='+',
-					help='file with CAN log')
+def initParser():
+	parser = argparse.ArgumentParser(
+		description='Parse CAN bus log',
+		epilog='If any questions - ask Andreyka'
+		)
 	
+	parser.add_argument('File', metavar='MyFile', nargs='?', help='file with CAN log')
+	parser.add_argument(
+					'-p',
+					'--protocol',
+					metavar ='Protocol',
+					nargs   ='?', 
+					choices = ['smartnet', 'kse'],
+					default ='smartnet',
+					#action='store_true',
+					help = 'Select protocol to parse'
+					)
+
+	return parser
+	
+def main():
+	
+	parser = initParser()
 	arg = parser.parse_args()
-	FileToParse = arg.File[0]
+	
+	FileToParse = arg.File
 	
 	pre, ext = os.path.splitext(FileToParse)
 	
@@ -20,7 +39,7 @@ def main():
 	
 	with open(FileToParse, 'r') as Input: content = Input.readlines()
 	
-	if True:
+	if arg.protocol == 'smartnet':
 		parseSmartNetProtocol(content, OutputFile)
 	else:
 		parseKseProtocol(content, OutputFile)
