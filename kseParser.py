01e3c91b11bea34974dccd9beabbb99cb51a50a5
Andrey.Vinogradov <andrey.vinogradov@teplomonitor.ru>
2022-04-21 16:42:21 +0300

    kseParser.py: working on parser

--- kseParser.py
+++ kseParser.py
@@ -1,3 +1,4 @@
+from asyncio import streams
 try:
 	from prettytable import PrettyTable
 except ImportError:
@@ -33,21 +34,81 @@ def getKseFuntionName(body):
 			 
 			return 'DHW Temperature = %s' %(temperatureValue)
 		
-	return 'Undefined'
+	return ' '
+
+def getParameterId(body):
+	if body[0] == 'FA':
+		return int(body[1] + body[2], 16)
+
+	return int(body[0], 16)
 	
-def getKseBodyDescription(body):
+
+def getParameterName(parameterId):
+	if parameterId in constantsKse.kseParameter:
+		return constantsKse.kseParameter[parameterId]['name']
+	
+	return '0x%X' % (parameterId)
+
+def getTemperatureValue(body):
+	value = int(body[0] + body[1], 16)
+	if value == 0x8000:
+		return 'Undefined'
+	if value == 0xFE70:
+		return 'Unused'
+
+	return '%0.1f' % (value/10.0)
+
+def getU8Value(body):
+	value = int(body[0], 16)
+	return '%d' % (value)
+
+def getBitValue(body):
+	value = int(body[0], 16)
+	return '0' if value == 0 else '1' 
+
+def getParameterValue(parameterId, body):
+	if parameterId in constantsKse.kseParameter:
+		if constantsKse.kseParameter[parameterId]['format'] == 'temperature':
+			return getTemperatureValue(body)
+		if constantsKse.kseParameter[parameterId]['format'] == 'u8':
+			return getU8Value(body)
+		if constantsKse.kseParameter[parameterId]['format'] == 'bit':
+			return getBitValue(body)
+	
+	return int(''.join(body), 16)
+
+def getKseBodyFormattedValue(parsedBody):
+	
+	parameterName  = getParameterName(parsedBody['parameter'])
+	messageType    = parsedBody['messageType']
+	toModule       = getKseModuleNameId(parsedBody['module'], parsedBody['id'])
+	
+	str = f'to {toModule:<15} {messageType}: {parameterName}'
+	
+	if messageType == 'req':
+		return str
+	
+	parameterValue = parsedBody['value']
+	
+	return f'{str} = {parameterValue}'
+
+def parseKseBody(body):
 	destModule   = int(body[0][0], 16)
 	messageType  = int(body[0][1], 16)
-	destModuleId = int(body[1]   , 16)
+	destModuleId = int(body[1]   , 16) & 0x0F
+	valuePos = 5 if body[2] == 'FA' else 3
 	
-	functionName = getKseFuntionName(body)
+	parameterId    = getParameterId   (body[2:])
+	parameterValue = getParameterValue(parameterId, body[valuePos:valuePos+2])
 	
-	toModule = getKseModuleNameId(destModule, destModuleId)
-	messageTypeStr = 'request' if messageType == 1 else 'response'
-	return ('to ' + toModule +
-		messageTypeStr + ' ' +
-		functionName)
-	
+	messageTypeStr = 'req' if messageType == 1 else 'res'
+	return {
+		'messageType': messageTypeStr,
+		'module'     : destModule,
+		'id'         : destModuleId, 
+		'parameter'  : parameterId,
+		'value'      : parameterValue,
+		}
 
 def prepareKseTable():
 	t = PrettyTable([	'T',
@@ -60,10 +121,11 @@ def prepareKseTable():
 					])
 	t.align = 'r'
 	t.align['Body'] = 'l'
+	t.align['Parsed body'] = 'l'
 	return t
 	
 
-def parseKseProtocolLine(line, t):
+def parseKseProtocolLine(line, t, filterBusId):
 	parseKseProtocolLine.i += 1
 	
 	try:
@@ -81,9 +143,14 @@ def parseKseProtocolLine(line, t):
 	delta     = pLine['deltaT']
 	
 	module, busId = parseKseHeader(header)
+	parsedBody    = parseKseBody(body)
 	
-	parsedHeader = getKseModuleNameId(module, busId)
-	parsedBody   = getKseBodyDescription(body)
+	if filterBusId != -1:
+		if (busId != filterBusId) and parsedBody['id'] != filterBusId:
+			return
+	
+	formattedHeader = getKseModuleNameId(module, busId)
+	formattedBody   = getKseBodyFormattedValue(parsedBody)
 	
 	headerStr = ' '.join(header)
 	bodyStr   = ' '.join(body)
@@ -93,16 +160,16 @@ def parseKseProtocolLine(line, t):
 				pLine['messageType'],
 				headerStr,
 				bodyStr,
-				parsedHeader,
-				parsedBody
+				formattedHeader,
+				formattedBody
 			])
 	
 #init static var
-parseKseProtocolLine.i         = 0
+parseKseProtocolLine.i = 0
 
-def parseKseProtocol(content, outputFile):
+def parseKseProtocol(content, outputFile, busId):
 	t = prepareKseTable()
 	for line in content:
-		parseKseProtocolLine(line, t)
+		parseKseProtocolLine(line, t, busId)
 	
 	with open(outputFile, 'w') as Output: Output.write(t.get_string())
